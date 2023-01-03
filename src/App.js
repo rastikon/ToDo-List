21dc426e68adb718bb5de57d620f820e7460d5c5
Rostyslav Kononenko <rastikon@gmail.com>
2023-01-03 17:45:13 +0100

    update something

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world!</p>
-        <a
-          className="App-link"
-          href="https://google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App__todo">
+        <div className="App__input-wrapper">
+          <input type={"text"} className="App__input" />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
